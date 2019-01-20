fcfa6b63b64cc43a262e2a3bb0fb42e6ec55dc5a
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-20 19:07:27 +0800

    練習三

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,14 +2,51 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Post;
 use App\Http\Requests;
 
-class AdminDashboardController extends Controller
+class AdminPostsController extends BaseController
 {
     public function index()
     {
-        return view('admin.dashboard.index');
+        return view('admin.posts.index');
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+        return view('admin.posts.index',$data);
+    }
+
+    //    public function index()
+//    {
+//        return view('admin.posts.index');
+//    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+    public function edit($id)
+    {
+        $post=Post::find($id);
+
+        $data=['post'=>$post];
+
+        return view('admin.posts.edit', $data);
+    }
+    public function store(PostRequest $request)
+    {
+        Post::create($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+
+    public function update(PostRequest $request,$id)
+    {
+        $post=Post::find($id);
+        $post->update($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
     }
 }
